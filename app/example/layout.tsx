ba6cc33f13699daf1dbefb3f07ded92766d2b661
pratap <pratapbantawa3@gmail.com>
2026-05-13 10:03:34 +0545

    Example

---
+++ app/example/layout.tsx
@@ -0,0 +1,13 @@
+export default function Layout({children}: {children: React.ReactNode}) {
+    return (
+        <section>
+            <div className="border p-4">Global Header</div>
+                {children}
+            <div className="border p-4">Global Footer</div>
+
+            <div className="border p-4 bg-gray-100">
+                {children}
+            </div>
+        </section>
+    );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="border p-4"> Root Path</div>
+  );
+}
